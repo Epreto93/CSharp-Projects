0b8da7093420fcd4651926010e7993d80746eb3b
EP <elzaniviljoen@gmail.com>
2021-07-20 10:46:34 +0200

    wpf project

---
+++ WpfAppMiniProjectApp/WpfAppMiniProject/AddressEntry.xaml.cs
@@ -0,0 +1,40 @@
+﻿using DemoLibrary;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WpfAppMiniProject
+{
+    /// <summary>
+    /// Interaction logic for AddressEntry.xaml
+    /// </summary>
+    public partial class AddressEntry : Window    
+    {
+        ISaveAddress _parent;
+        public AddressEntry(ISaveAddress parent)
+        {
+            InitializeComponent();
+            _parent = parent;
+        }
+
+        private void saveAddress_Click(object sender, RoutedEventArgs e)
+        {
+            AddressModel address = new AddressModel
+            {
+                streetName = streetText.Text,
+                townName = townText.Text
+            };
+            _parent.SaveAddress(address);
+            this.Close();
+
+        }
+    }
+}
---
+++ WpfAppMiniProjectApp/WpfAppMiniProject/MainWindow.xaml.cs
@@ -0,0 +1,61 @@
+﻿using DemoLibrary;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfAppMiniProject
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window,ISaveAddress
+    {
+        BindingList<AddressModel> addresses = new BindingList<AddressModel>();
+        public MainWindow()
+        {
+
+               
+            InitializeComponent();
+
+           
+            addressesList.ItemsSource = addresses;
+        
+        }
+
+        public void SaveAddress(AddressModel address)
+        {
+            addresses.Add(address);
+        }
+
+        private void addAddressButton_Click(object sender, RoutedEventArgs e)
+        {
+            AddressEntry entry= new AddressEntry(this);
+            entry.Show();
+
+        }
+
+        private void savePerson_Click(object sender, RoutedEventArgs e)
+        {
+            PersonModel person = new PersonModel
+            {
+                Name = firstNameText.Text,
+                Surname = lastNameText.Text,
+                Addresses = addresses.ToList()
+
+
+            };
+        }
+    }
+}
